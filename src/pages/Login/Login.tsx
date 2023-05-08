534154cf9eb470be02a36a1a510512817ff8229a
grace <graceguo.nz@gmail.com>
2023-05-08 12:53:37 +1200

    fix login bug and adjust layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -28,14 +28,12 @@ export function Login() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const role = await login(username, password);
-    if (role) {
+    if (role && role === "Customer") {
       localStorage.setItem(AUTHENTICATED_ROLE, role); // Store the authentication status in local storage
       localStorage.setItem(AUTHENTICATED_USER, username); // Store the authentication status in local storage
-      if (role === "Customer") {
-        navigate("/products");
-      } else {
-        error();
-      }
+      navigate("/products");
+    } else {
+      error();
     }
   };
 
@@ -43,45 +41,51 @@ export function Login() {
     <>
       {contextHolder}
       <div className="flex justify-center items-center h-screen">
-        <form
-          onSubmit={handleSubmit}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3"
-        >
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <div className="flex items-center justify-end">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+        <div className="w-1/3 flex flex-col gap-4">
+          <div className="font-bold text-xl text-center">Customer Login</div>
+          <form onSubmit={handleSubmit} className="bg-white shadow-md rounded p-8">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-end">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
